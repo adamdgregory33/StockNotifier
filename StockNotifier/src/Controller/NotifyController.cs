b61e537f6be2bb30cbfa942806beffa1bbf89335
Adam Gregory <adamdgregory33@users.noreply.github.com>
2024-02-18 17:13:48 +0000

    Initial Controller files

---
+++ StockNotifier/src/Controller/NotifyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockNotifier.src.Controller
+{
+    [Route("/v1/api/[controller]")]
+    [ApiController]
+    public class NotifyController : ControllerBase
+    {
+        // POST api/<NotifyController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            Console.WriteLine(value);
+        }
+
+    }
+}
